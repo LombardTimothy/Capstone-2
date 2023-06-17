3bc4a667420d86e6bf474935f6b8aafc96d2d499
DKamalie <46668318+DKamalie@users.noreply.github.com>
2023-06-17 21:58:16 +0200

    Employee Controller test and pom update

---
+++ PizzaDelivery/src/test/java/za/ac/cput/controller/EmployeeControllerTest.java
@@ -0,0 +1,73 @@
+package za.ac.cput.controller;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import za.ac.cput.domain.Employee;
+import za.ac.cput.factory.EmployeeFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.MethodName.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class EmployeeControllerTest {
+    private static Employee employee = EmployeeFactory.createEmployee("0012", "Basil", "Roode");
+    @Autowired
+    private TestRestTemplate restTemplate;
+    private String baseURL = "http://localhost:8080/employee";
+
+    @Test
+    void a_create() {
+        String url = baseURL + "/create";
+//        ResponseEntity<Employee> postResponse = restTemplate.postForEntity(url, employee,);
+//        assertNotNull(postResponse);
+//        assertNotNull(postResponse.getBody());
+//        employee = postResponse.getBody();
+        System.out.println("Saved data: " + employee);
+//        assertEquals(employee.getEmpId(), postResponse.getBody().getEmpId);
+     }
+
+    @Test
+    void b_read() {
+        String url = baseURL + "/read/" + employee.getEmpId();
+        System.out.println("URL: " + url);
+        ResponseEntity<Employee> response = restTemplate.getForEntity(url, Employee.class);
+        assertEquals(employee.getEmpId(), response.getBody().getEmpId());
+        System.out.println(response.getBody());
+    }
+
+    @Test
+    void c_update() {
+//        Employee updated = new Employee.Builder().copy(employee).setEmpName("Josepheen");
+        String url = baseURL + "/update";
+        System.out.println("URL : " + url);
+//        System.out.println("Post data: " + updated);
+//        ResponseEntity<Employee> response = restTemplate.postForEntity(url, updated, Employee);
+//        assertNotNull(response.getBody());
+    }
+
+    @Test
+    void e_delete() {
+//        String url = baseUrl + "/delete/" + employee.getEmpId();
+//        System.out.println("URL: " + url);
+//        restTemplate.delete(url);
+    }
+
+    @Test
+    void d_getall() {
+        String url = baseURL + "/getall";
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+//        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity);
+//        System.out.println("Show All:");
+//        System.out.println(response);
+//        System.out.println(response.getBody());
+    }
+}
