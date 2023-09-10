cce08fdc8b5f93ca1e62af8eb38f22d20f8b9243
Timothy <220154856@mycput.ac.za>
2023-09-10 17:28:50 +0200

    Edited OrderLine Factory and Controller.

--- PizzaDelivery/src/test/java/za/ac/cput/controller/OrderLineControllerTest.java
+++ PizzaDelivery/src/test/java/za/ac/cput/controller/OrderLineControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import za.ac.cput.domain.*;
 import za.ac.cput.factory.*;
 
@@ -35,9 +36,10 @@ class OrderLineControllerTest {
     private static Address address1 = AddressFactory.buildAddress("21", "Jump Street", "West Olmstead", "Bikini Bottom", "California", "Crownlands", "0007", AddressType.RESIDENTIAL_HOME);
     private static Customer customer1 = CustomerFactory.buildCustomer("Rickon", "Stark", "0868561233", address1);
     private static Pizzeria pizzeria = PizzeriaFactory.buildPizzaria("Hill Crest","Hotel Transalvania");
+    private static Pizzeria pizzeria1 = PizzeriaFactory.buildPizzaria("Hill Crest","Hotel Transalvania");
     private static Order order1 = OrderFactory.createOrder(date1, time1, customer1, Order.OrderStatus.NEW, pizzeria);
     private static Base base = BaseFactory.buildBase( Base.BaseCrust.CRUSTY, Base.BaseThickness.THIN, Base.BaseTexture.CRISPY, 20);
-    private static Pizza pizza = PizzaFactory.createPizza(base, "Margherita pizza", "Thin crust with high quality flour and fresh tomato sauce and with creamy extra cheese.", Pizza.Size.SMALL, false, 55, pizzeria);
+    private static Pizza pizza = PizzaFactory.createPizza(base, "Margherita pizza", "Thin crust with high quality flour and fresh tomato sauce and with creamy extra cheese.", Pizza.Size.SMALL, false, 55, pizzeria1);
     private static Bill bill = BillFactory.createBill(75);
 
     private static OrderLine orderLine = OrderLineFactory.buildOrderLine(4, order1, pizza, bill);
@@ -48,16 +50,17 @@ class OrderLineControllerTest {
     private final String baseURL = "http://localhost:8080/orderline";
 
     @Test
+    @Transactional
     public void a_create() {
-        String url = baseURL + "/create";
-        ResponseEntity<OrderLine> postResponse = restTemplate.postForEntity(url, orderLine, OrderLine.class);
-        assertNotNull(postResponse);
-        assertNotNull(postResponse.getBody());
 
-        OrderLine savedOrderLine = postResponse.getBody();
-        System.out.println("Saved data: \n" + savedOrderLine);
-        assertEquals(orderLine.getOrderLineId(), savedOrderLine.getOrderLineId());
-        postResponse.getStatusCode();
+            String url = baseURL + "/create";
+            ResponseEntity<OrderLine> postResponse = restTemplate.postForEntity(url, orderLine, OrderLine.class);
+            assertNotNull(postResponse);
+            assertNotNull(postResponse.getBody());
+
+            OrderLine savedOrderLine = postResponse.getBody();
+            System.out.println("Saved data: \n" + savedOrderLine);
+         assertEquals(orderLine.getOrderLineId(), savedOrderLine.getOrderLineId());
     }
 
     @Test
