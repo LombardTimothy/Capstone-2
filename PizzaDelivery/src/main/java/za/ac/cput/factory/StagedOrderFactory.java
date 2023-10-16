e50ca8381c1ce1c80969622dd2709c1e6b0af817
keenan12390 <63111603+keenan12390@users.noreply.github.com>
2023-10-16 18:53:30 +0200

    Updated StagedOrder.java bit service has failures

---
+++ PizzaDelivery/src/main/java/za/ac/cput/factory/StagedOrderFactory.java
@@ -0,0 +1,39 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.*;
+import za.ac.cput.util.Helper;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/* OrderFactory.java
+ Author: Keenan Meyer (220194920)
+ Date: 16 October (last updated) 2023
+*/
+
+    public class StagedOrderFactory {
+        public static StagedOrder createOrder(LocalDate createDate, LocalTime time, Customer customer, String qauntity, Pizza pizza, double total, StagedOrder.OrderStatus orderStatus, Pizzeria pizzeria) {
+            if (Helper.isNullOrEmpty(String.valueOf(Helper.isNullOrEmpty(String.valueOf(createDate)))) || Helper.isNullOrEmpty(String.valueOf(time))  || Helper.isNullOrEmpty(String.valueOf(customer)) || Helper.isNullOrEmpty(String.valueOf(orderStatus)) || Helper.isNullOrEmpty(String.valueOf(pizzeria)))  {
+                return null;
+            }
+
+            int orderId = Helper.generateId2();
+
+            StagedOrder stagedOrder = new StagedOrder
+                    .Builder()
+                    .setOrderId(orderId)
+                    .setCreateDate(createDate)
+                    .setTime(time)
+                    .setCustomer(customer)
+                    .setQuantity(qauntity)
+                    .setPizza(pizza)
+                    .setTotal(total)
+                    .setOrderStatus(orderStatus)
+                    .setPizzeria(pizzeria)
+                    .build();
+            return stagedOrder;
+
+        }
+
+    }
+
